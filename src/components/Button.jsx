4539a385031b7a6d40c2c269ef704cb0a339c663
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-12-14 15:54:50 +0530

    added changes to components directory structure

---
+++ src/components/Button.jsx
@@ -0,0 +1,26 @@
+export default function PrimaryButton({
+  width,
+  height,
+  backgroundColor,
+  color,
+  border,
+  borderColor,
+  fontSize,
+  buttonText,
+}) {
+  return (
+    <button
+      style={{
+        width,
+        height,
+        backgroundColor,
+        color,
+        border,
+        borderColor,
+        fontSize,
+      }}
+    >
+      {buttonText}
+    </button>
+  );
+}
---
+++ src/components/Input.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const InputField = ({ placeholder, type, value, name, change }) => {
+  return (
+    <div className="mb-3 pt-0">
+      <input
+        type={type}
+        value={value}
+        name={name}
+        placeholder={placeholder}
+        onChange={change}
+      />
+    </div>
+  );
+};
+
+export default InputField;
