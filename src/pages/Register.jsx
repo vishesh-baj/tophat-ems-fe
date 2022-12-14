a8ff641b648a9dabb1b0642fab083b3f142c8de0
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-12-14 12:20:48 +0530

    implemented redux

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
     <div>
-        <div><h1>Signup</h1></div>
+      <div>
+        <h1>Signup</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
