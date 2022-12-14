565bda400de1bec8be61be9083011d8287678641
manishtophat <manish.tophat@gmail.com>
2022-12-14 17:44:31 +0530

    added comment to loginm page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,7 +7,7 @@ const schema = yup.object().shape({
   email: yup.string().email().required(),
   password: yup.string().min(8).max(32).required(),
 });
-
+// login page
 const Login = () => {
   const {
     register,
